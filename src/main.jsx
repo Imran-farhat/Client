c8a494c586bbf56f5bea36508a366caf02069ff9
Imran Farhat <imranfarhat442@gmail.com>
2026-05-23 23:46:02 +0530

    modified:   src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App';
 import './styles/index.css';
 
+console.log("DEBUG: VITE_GOOGLE_CLIENT_ID is =>", import.meta.env.VITE_GOOGLE_CLIENT_ID);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || "missing-client-id"}>
