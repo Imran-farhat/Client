cccc0d415681e740f996d5d16b22f0cd02cff430
Imran Farhat <imranfarhat442@gmail.com>
2026-07-18 12:07:50 +0530

    ci: trigger rebuild to load newly configured repository secrets

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Build trigger update: Secrets configured
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -9,3 +10,4 @@ export default defineConfig({
     }
   }
 });
+
