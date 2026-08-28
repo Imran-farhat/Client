a342ef0bdea55868f363f63d6521cdf2fec41616
Imran-farhat <117956753+Imran-farhat@users.noreply.github.com>
2026-08-28 12:53:50 +0000

    Deploying to gh-pages from @ Imran-farhat/Client@bd918ec42dc1f08237399f0cb955109e9382b5d4 🚀

--- assets/IDCard-BOOFk1hw.js
+++ assets/IDCard-BOOFk1hw.js
@@ -1,4 +1,4 @@
-import{r as Xe,j as w}from"./index-1gjeKd49.js";/*!
+import{r as Xe,j as w}from"./index-Bu_uc_BX.js";/*!
  * html2canvas 1.4.1 <https://html2canvas.hertzen.com>
  * Copyright (c) 2022 Niklas von Hertzen <https://hertzen.com>
  * Released under MIT License
--- assets/constants-9UwnhYa3.js
+++
@@ -1,1 +0,0 @@
-const a="https://thennindiaweldingthozhilaalargalnalasangam.org",n="dejryogbg",s="member_preset";export{n as C,a as S,s as a};
