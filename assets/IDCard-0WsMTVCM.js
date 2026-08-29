165d61705130e51d88fd6c1229cef87c3962d295
Imran-farhat <117956753+Imran-farhat@users.noreply.github.com>
2026-08-29 17:36:45 +0000

    Deploying to gh-pages from @ Imran-farhat/Client@62f705d0749e384c85a4e8c0810b811bd635eddd 🚀

--- assets/IDCard-0WsMTVCM.js
+++ assets/IDCard-0WsMTVCM.js
@@ -1,4 +1,4 @@
-import{r as Xe,j as w}from"./index-BHEn_wU-.js";/*!
+import{r as Xe,j as w}from"./index-DeZY3vPs.js";/*!
  * html2canvas 1.4.1 <https://html2canvas.hertzen.com>
  * Copyright (c) 2022 Niklas von Hertzen <https://hertzen.com>
  * Released under MIT License
